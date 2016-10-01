09818029c16765ef1d098454d9c90d210fdf1dc4
leosorgetz <leosorgetz123@gmail.com>
2016-10-01 14:20:36 -0300

    Adicionado bootstrap na pasta assets

---
+++ application/views/template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Projeto APAE</title>
+	<link rel="stylesheet" href="<?php echo site_url('assets/bootstrap/css/bootstrap.min.css');?>">
+</head>
+<body>
+	<div class="" id="contents">
+		<? echo $contents ?>
+	</div>
+</body>
+<footer> 
+	<script src="<?php echo site_url('assets/js/jquery-3.1.1.min.js');?>"></script>
+	<script src="<?php echo site_url('assets/bootstrap/js/bootstrap.min.js');?>"></script>
+</footer>
+</html>
---
+++ application/views/teste.php
@@ -0,0 +1,1 @@
+<h1>Testando Template + URL</h1>
