cf53cb5484b6305e8c8c9474f45f2dbf9d9c7b2f
Gabriel Dewes <gabrieldewes>
2016-12-07 21:47:07 -0200

    fix template

---
+++ application/views/layout/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+  <div class="container">
+    <p class="text-muted">
+      <script type="text/javascript">
+        document.write(new Date().getFullYear());
+      </script>
+      &copy;
+      &middot;
+      <a href="#">APAE</a>
+       &middot;
+      <a href="#">LDS</a>
+    </p>
+  </div>
+</footer>
