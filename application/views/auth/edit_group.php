4503d4579c9b30dda420ccfbc061095e9c8bbb03
Gabriel Dewes <Gabriel Dewes>
2017-01-10 11:11:41 -0200

    edit _group com estilo

--- application/views/auth/edit_group.php
+++ application/views/auth/edit_group.php
@@ -1,20 +1,24 @@
-<h1><?php echo lang('edit_group_heading');?></h1>
-<p><?php echo lang('edit_group_subheading');?></p>
+<div class="well well-lg">
+  <div class="page-header">
+    <h2><?php echo lang('edit_group_heading');?></h2>
+    <p><?php echo lang('edit_group_subheading');?></p>
 
-<div id="infoMessage"><?php echo $message;?></div>
+    <div id="infoMessage"><?php echo $message;?></div>
+  </div>
 
-<?php echo form_open(current_url());?>
+  <?php echo form_open(current_url());?>
 
-      <p>
-            <?php echo lang('edit_group_name_label', 'group_name');?> <br />
-            <?php echo form_input($group_name);?>
-      </p>
+        <p class="col-md-6">
+              <?php echo lang('edit_group_name_label', 'group_name');?> <br />
+              <?php echo form_input($group_name);?>
+        </p>
 
-      <p>
-            <?php echo lang('edit_group_desc_label', 'description');?> <br />
-            <?php echo form_input($group_description);?>
-      </p>
+        <p class="col-md-6">
+              <?php echo lang('edit_group_desc_label', 'description');?> <br />
+              <?php echo form_input($group_description);?>
+        </p>
 
-      <p><?php echo form_submit('submit', lang('edit_group_submit_btn'));?></p>
+        <p><?php echo form_submit('submit', lang('edit_group_submit_btn'), "class='btn btn-success'");?></p>
 
-<?php echo form_close();?>
+  <?php echo form_close();?>
+</div>
