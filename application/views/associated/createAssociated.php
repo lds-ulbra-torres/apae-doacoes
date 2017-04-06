a56adde9fdce4d6d7bf762ba894713c4750012c2
Eduardo Marques <edums@live.com>
2017-04-06 19:55:06 -0300

    re-alinhados os códigos sem mudanças

--- application/views/associated/createAssociated.php
+++ application/views/associated/createAssociated.php
@@ -10,11 +10,7 @@
           <?= form_error('uuid_associate','<div class="row"><div class="alert alert-danger alert-dismissible col-md-8 col-md-offset-1" role="alert">', '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div>') ?>
           <label for="uuid_associate" class="col-sm-3 col-form-label">Código Único</label>
           <div class="col-sm-8">
-            <input type="text" class="form-control" onchange="close('#uuidError')"
-            id="uuid_associate" name="uuid_associate"
-            value="<?= set_value('uuid_associate', isset($associate->uuid_associate) ? $associate->uuid_associate:''); ?>"
-            placeholder="Código Universal Único"
-            required />
+            <input type="text" class="form-control" onchange="close('#uuidError')" id="uuid_associate" name="uuid_associate" value="<?= set_value('uuid_associate', isset($associate->uuid_associate) ? $associate->uuid_associate:''); ?>" placeholder="Código Universal Único" required />
           </div>
         </div>
 
@@ -22,10 +18,7 @@
           <?= form_error('name_associate','<div class="row"><div class="alert alert-danger alert-dismissible col-md-8 col-md-offset-1" role="alert">', '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div>') ?>
           <label for="name_associate" class="col-sm-3 col-form-label">Nome Completo</label>
           <div class="col-sm-8">
-            <input type="text" class="form-control" onchange="close('#nameError')"
-              id="name_associate" name="name_associate"
-              value="<?= set_value('name_associate', isset($associate->name_associate) ? $associate->name_associate:''); ?>"
-              placeholder="Nome Completo">
+            <input type="text" class="form-control" onchange="close('#nameError')" id="name_associate" name="name_associate" value="<?= set_value('name_associate', isset($associate->name_associate) ? $associate->name_associate:''); ?>" placeholder="Nome Completo">
           </div>
         </div>
 
@@ -33,10 +26,7 @@
           <?= form_error('birth_date','<div class="row"><div class="alert alert-danger alert-dismissible col-md-8 col-md-offset-1" role="alert">', '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div>') ?>
           <label for="birth_date" class="col-sm-3 col-form-label">Aniversário</label>
           <div class="col-sm-8">
-            <input type="date" class="form-control" onchange="close('#birthError')"
-              id="birth_date" name="birth_date"
-              value="<?= set_value('birth_date', isset($associate->birth_date) ? $associate->birth_date:''); ?>"
-              required />
+            <input type="date" class="form-control" onchange="close('#birthError')" id="birth_date" name="birth_date" value="<?= set_value('birth_date', isset($associate->birth_date) ? $associate->birth_date:''); ?>" required />
           </div>
         </div>
 
@@ -44,10 +34,7 @@
           <?= form_error('rg','<div class="row"><div class="alert alert-danger alert-dismissible col-md-8 col-md-offset-1" role="alert">', '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div>') ?>
           <label for="rg" class="col-sm-3 col-form-label">RG</label>
           <div class="col-sm-8">
-            <input type="number" class="form-control"
-              id="rg" name="rg" placeholder="RG" onchange="close('#rgError')"
-              value="<?= set_value('rg', isset($associate->rg) ? $associate->rg:''); ?>"
-              placeholder="RG" />
+            <input type="number" class="form-control" id="rg" name="rg" placeholder="RG" onchange="close('#rgError')" value="<?= set_value('rg', isset($associate->rg) ? $associate->rg:''); ?>" placeholder="RG" />
           </div>
         </div>
 
@@ -76,39 +63,28 @@
         <div class="form-group row">
           <label for="street" class="col-sm-3 col-form-label">CEP</label>
           <div class="col-sm-8">
-            <input type="text" class="form-control"
-              id="cep" name="cep" placeholder="Cep"
-              value="<?= set_value('cep', isset($associate->cep) ? $associate->cep:''); ?>">
+            <input type="text" class="form-control" id="cep" name="cep" placeholder="Cep" value="<?= set_value('cep', isset($associate->cep) ? $associate->cep:''); ?>">
           </div>
         </div>
 
         <div class="form-group row">
           <label for="street" class="col-sm-3 col-form-label">Rua</label>
           <div class="col-sm-8">
-            <input type="text" class="form-control"
-              id="street" name="street"
-              placeholder="Nome da Rua"
-              value="<?= set_value('street', isset($associate->street) ? $associate->street:''); ?>">
+            <input type="text" class="form-control" id="street" name="street" placeholder="Nome da Rua" value="<?= set_value('street', isset($associate->street) ? $associate->street:''); ?>">
           </div>
         </div>
 
         <div class="form-group row">
           <label for="number" class="col-sm-3 col-form-label">Número</label>
           <div class="col-sm-8">
-            <input type="number" class="form-control"
-              id="number" name="number"
-              placeholder="Número da Residência"
-              value="<?= set_value('number', isset($associate->number) ? $associate->number:''); ?>">
+            <input type="number" class="form-control" id="number" name="number" placeholder="Número da Residência" value="<?= set_value('number', isset($associate->number) ? $associate->number:''); ?>">
           </div>
         </div>
 
         <div class="form-group row">
           <label for="neighborhood" class="col-sm-3 col-form-label">Bairro</label>
           <div class="col-sm-8">
-            <input type="text" class="form-control"
-              id="neighborhood" name="neighborhood"
-              placeholder="Bairro"
-              value="<?= set_value('neighborhood', isset($associate->neighborhood) ? $associate->neighborhood:''); ?>">
+            <input type="text" class="form-control" id="neighborhood" name="neighborhood" placeholder="Bairro" value="<?= set_value('neighborhood', isset($associate->neighborhood) ? $associate->neighborhood:''); ?>">
           </div>
         </div>
 
@@ -131,10 +107,7 @@
           <?= form_error('duo_date','<div class="row"><div class="alert alert-danger alert-dismissible col-md-8 col-md-offset-1" role="alert">', '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div>') ?>
           <label for="duo_date" class="col-sm-3 col-form-label">Data Venc. 1ª Doação</label>
           <div class="col-sm-8">
-            <input type="date" class="form-control"
-              id="duo_date" name="duo_date"
-              value="<?=date('Y-m-d', strtotime('+1 month'))?>"
-              required />
+            <input type="date" class="form-control" id="duo_date" name="duo_date" value="<?=date('Y-m-d', strtotime('+1 month'))?>" required />
           </div>
         </div>
 
@@ -142,17 +115,12 @@
           <?= form_error('value_frequency','<div class="row"><div class="alert alert-danger alert-dismissible col-md-8 col-md-offset-1" role="alert">', '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div>') ?>
           <label for="value_frequency" class="col-sm-3 col-form-label">Valor de Doação</label>
           <div class="col-sm-8">
-            <input type="text" class="form-control"
-            id="value_frequency" name="value_frequency" placeholder="Valor"
-            value="<?= set_value('value_frequency', isset($associate->value_frequency) ? $associate->value_frequency:''); ?>"
-            required />
+            <input type="text" class="form-control" id="value_frequency" name="value_frequency" placeholder="Valor" value="<?= set_value('value_frequency', isset($associate->value_frequency) ? $associate->value_frequency:''); ?>" required />
           </div>
         </div>
 
-
         <div class="form-group row">
           <?= form_error('id_payment_type','<div class="row"><div class="alert alert-danger alert-dismissible col-md-8 col-md-offset-1" role="alert">', '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div>') ?>
-
           <label for="id_payment_type" class="col-sm-3 col-form-label">Tipo de Pagamento</label>
           <div class="col-sm-8">
             <select required class="form-control" name="id_payment_type" id="id_payment_type" >
@@ -185,30 +153,21 @@
           <div class="form-group row">
             <label for="name_in_card" class="col-sm-3 col-form-label">Nome no Cartão</label>
             <div class="col-sm-8">
-              <input type="text" class="form-control"
-                id="name_in_card" name="name_in_card"
-                placeholder="Nome no Cartão"
-                value="<?= set_value('name_in_card', isset($associate->name_in_card) ? $associate->name_in_card:''); ?>">
+              <input type="text" class="form-control" id="name_in_card" name="name_in_card" placeholder="Nome no Cartão" value="<?= set_value('name_in_card', isset($associate->name_in_card) ? $associate->name_in_card:''); ?>">
             </div>
           </div>
 
           <div class="form-group row">
             <label for="agency_number" class="col-sm-3 col-form-label">Número da Agência</label>
             <div class="col-sm-8">
-              <input type="text" class="form-control"
-                id="agency_number" name="agency_number"
-                placeholder="Número da Agância"
-                value="<?= set_value('agency_number', isset($associate->agency_number) ? $associate->agency_number:''); ?>">
+              <input type="text" class="form-control" id="agency_number" name="agency_number" placeholder="Número da Agância" value="<?= set_value('agency_number', isset($associate->agency_number) ? $associate->agency_number:''); ?>">
             </div>
           </div>
 
           <div class="form-group row">
             <label for="account_number" class="col-sm-3 col-form-label">Número da Conta</label>
             <div class="col-sm-8">
-              <input type="text" class="form-control"
-                id="account_number" name="account_number"
-                placeholder="Número da Conta"
-                value="<?= set_value('account_number', isset($associate->account_number) ? $associate->account_number:''); ?>">
+              <input type="text" class="form-control" id="account_number" name="account_number" placeholder="Número da Conta" value="<?= set_value('account_number', isset($associate->account_number) ? $associate->account_number:''); ?>">
             </div>
           </div>
         </div>
@@ -233,9 +192,7 @@
 
     </form>
   </div>
-
 </div>
-
 
 <div class="modal fade" id="contact_modal">
   <div class="modal-dialog" role="document">
