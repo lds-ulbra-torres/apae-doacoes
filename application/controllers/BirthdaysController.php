77510526aeb4e234e1019d5cf9a1a5eb49b5a3d7
JozielOliveira <programdor.joziel@gmail.com>
2018-11-19 14:05:39 -0200

    email

--- application/controllers/BirthdaysController.php
+++ application/controllers/BirthdaysController.php
@@ -142,8 +142,8 @@ class BirthdaysController extends CI_Controller {
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->SMTPSecure = 'tls';
-    $mail->Username = 'apaedetorres@gmail.com';
-    $mail->Password = 'apae2533';
+    $mail->Username = 'usuario101050@gmail.com';
+    $mail->Password = 'Marcilio1';
     $mail->Port = 587;
     $mail->CharSet   = 'utf-8';
     $mail->setFrom('apae@gmail.com');
